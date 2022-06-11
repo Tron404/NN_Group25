c908a4a1b10286a767082da701e7df5f7f4408f4
GreetjeSam <78415950+GreetjeSam@users.noreply.github.com>
2022-06-11 10:22:49 +0200

    Update miditodata.py

--- drum_midi/miditodata.py
+++ drum_midi/miditodata.py
@@ -7,10 +7,12 @@ import pathlib
 import pandas as pd
 import pretty_midi
 
+# directory with the .mid files
 data_dir = pathlib.Path('data/groove-v1.0.0-midionly/groove')
 
+# takes .mid filenames for printing
 filenames = glob.glob(str(data_dir/'*.mid*'))
-print('Number of files:', len(filenames))
+print('Number of files:', len(filenames))                   
 
 for file in filenames[:]:
 
@@ -18,11 +20,13 @@ for file in filenames[:]:
 
     pm = pretty_midi.PrettyMIDI(file)
 
+    # recognizes that the instrument is drums
     print('Number of instruments:', len(pm.instruments))
     instrument = pm.instruments[0]
     if instrument.is_drum:
         print('Instrument name: drums')
 
+    # tranform the .mid to the notes
     def midi_to_notes(midi_file: str) -> pd.DataFrame:
         pm = pretty_midi.PrettyMIDI(midi_file)
         instrument = pm.instruments[0]
@@ -32,6 +36,7 @@ for file in filenames[:]:
         sorted_notes = sorted(instrument.notes, key=lambda note: note.start)
         prev_start = sorted_notes[0].start
 
+        # obtain relevant data per note
         for note in sorted_notes:
             start = note.start
             end = note.end
